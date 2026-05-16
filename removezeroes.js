a45c2ece029412c8d289dea71b54b001c5e8336d
PRINCE SHARMA <princekumar468543@gmail.com>
2026-05-16 23:33:12 +0530

    Add files via upload

---
+++ removezeroes.js
@@ -0,0 +1,13 @@
+let nums = [0,1,0,3,12]
+
+let j = 0;
+ for(let i = 0; i<nums.length;i++){
+    if(nums[i]!== 0 ){
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        j++
+    }
+     }
+
+     console.log(nums);
